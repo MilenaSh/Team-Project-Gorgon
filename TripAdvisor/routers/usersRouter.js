dcc6c1b08d89a47b4df65596183719fa52950c0b
vixataaa <vmetodiev93@gmail.com>
2017-05-05 13:21:36 +0300

    Add user authentication

--- TripAdvisor/routers/usersRouter.js
+++ TripAdvisor/routers/usersRouter.js
@@ -3,6 +3,7 @@ var express = require('express');
 module.exports = function(db) {
     var router = express.Router();
 
+    // Register new user
     router.post('/', function(req, res) {
         const users = db.get('users');
 
@@ -24,6 +25,29 @@ module.exports = function(db) {
         res.json({
             username: userToRegister.username,
             userID: userToRegister.id
+        });
+    });
+
+    // Authenticate user
+    router.put('/', function(req, res) {
+        const users = db.get('users');
+
+        const userToLogIn = req.body;
+
+        const foundUser = users.find({
+            username: userToLogIn.username,
+            passHash: userToLogIn.passHash
+        }).value();
+
+        if(!foundUser) {
+            res.status(401)
+                .json("Authentication failed.");
+            return;
+        }
+
+        res.json({
+            username: foundUser.username,
+            userID: foundUser.id
         });
     });
 
