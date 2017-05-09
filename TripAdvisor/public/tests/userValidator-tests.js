2e93dc037a139051d519007ba179e61cacab0d9f
vixataaa <vmetodiev93@gmail.com>
2017-05-09 05:09:23 +0300

    Generic requester tests

--- TripAdvisor/public/tests/userValidator-tests.js
+++ TripAdvisor/public/tests/userValidator-tests.js
@@ -1,242 +1,255 @@
-import { userValidator } from 'userValidator';
+import {userValidator} from 'userValidator';
 const expect = chai.expect;
 
-describe('userValidator tests', function() {
-  it('validateUser should not throw when a valid user is passed', function() {
-    const user = {
-        username: 'validUser',
-        email: 'valid@email.com',
-        password: 'validPassword',
-        confirmPassword: 'validPassword',
-        secretQuestion: 'aValidQuestion',
-        secretAnswer: 'aValidAnswer'
-    };
+describe('userValidator tests', function () {
+    it('validateUser should not throw when a valid user is passed', function () {
+        const user = {
+            username: 'validUser',
+            email: 'valid@email.com',
+            password: 'validPassword',
+            confirmPassword: 'validPassword',
+            secretQuestion: 'aValidQuestion',
+            secretAnswer: 'aValidAnswer'
+        };
 
-    const action = function() {
-        userValidator().validateUser(user);
-    };
+        const action = function () {
+            userValidator().validateUser(user);
+        };
 
-    expect(action).to.not.throw();
-  });
+        expect(action)
+            .to
+            .not
+            .throw();
+    });
 
-  
-  it('validateUser should throw when user with empty username is passed', function() {
-    const user = {
-        username: '',
-        email: 'valid@email.com',
-        password: 'validPassword',
-        confirmPassword: 'validPassword',
-        secretQuestion: 'aValidQuestion',
-        secretAnswer: 'aValidAnswer'
-    };
+    it('validateUser should throw when user with empty username is passed', function () {
+        const user = {
+            username: '',
+            email: 'valid@email.com',
+            password: 'validPassword',
+            confirmPassword: 'validPassword',
+            secretQuestion: 'aValidQuestion',
+            secretAnswer: 'aValidAnswer'
+        };
 
-    const action = function() {
-        userValidator().validateUser(user);
-    };
+        const action = function () {
+            userValidator().validateUser(user);
+        };
 
-    expect(action).to.throw('Username cannot be empty');
-  });
+        expect(action)
+            .to
+            .throw('Username cannot be empty');
+    });
 
-    
-  it('validateUser should throw when user with short username is passed', function() {
-    const user = {
-        username: 'u',
-        email: 'valid@email.com',
-        password: 'validPassword',
-        confirmPassword: 'validPassword',
-        secretQuestion: 'aValidQuestion',
-        secretAnswer: 'aValidAnswer'
-    };
+    it('validateUser should throw when user with short username is passed', function () {
+        const user = {
+            username: 'u',
+            email: 'valid@email.com',
+            password: 'validPassword',
+            confirmPassword: 'validPassword',
+            secretQuestion: 'aValidQuestion',
+            secretAnswer: 'aValidAnswer'
+        };
 
-    const action = function() {
-        userValidator().validateUser(user);
-    };
+        const action = function () {
+            userValidator().validateUser(user);
+        };
 
-    expect(action).to.throw('more than');
-  });
+        expect(action)
+            .to
+            .throw('more than');
+    });
 
-      
-  it('validateUser should throw when user with non-latin username is passed', function() {
-    const user = {
-        username: 'здр,кпр',
-        email: 'valid@email.com',
-        password: 'validPassword',
-        confirmPassword: 'validPassword',
-        secretQuestion: 'aValidQuestion',
-        secretAnswer: 'aValidAnswer'
-    };
+    it('validateUser should throw when user with non-latin username is passed', function () {
+        const user = {
+            username: 'здр,кпр',
+            email: 'valid@email.com',
+            password: 'validPassword',
+            confirmPassword: 'validPassword',
+            secretQuestion: 'aValidQuestion',
+            secretAnswer: 'aValidAnswer'
+        };
 
-    const action = function() {
-        userValidator().validateUser(user);
-    };
+        const action = function () {
+            userValidator().validateUser(user);
+        };
 
-    expect(action).to.throw('latin');
-  });  
+        expect(action)
+            .to
+            .throw('latin');
+    });
 
-    
-  it('validateUser should throw when user with empty email is passed', function() {
-    const user = {
-        username: 'validUsername',
-        email: '',
-        password: 'validPassword',
-        confirmPassword: 'validPassword',
-        secretQuestion: 'aValidQuestion',
-        secretAnswer: 'aValidAnswer'
-    };
+    it('validateUser should throw when user with empty email is passed', function () {
+        const user = {
+            username: 'validUsername',
+            email: '',
+            password: 'validPassword',
+            confirmPassword: 'validPassword',
+            secretQuestion: 'aValidQuestion',
+            secretAnswer: 'aValidAnswer'
+        };
 
-    const action = function() {
-        userValidator().validateUser(user);
-    };
+        const action = function () {
+            userValidator().validateUser(user);
+        };
 
-    expect(action).to.throw('cannot be empty');
-  });
+        expect(action)
+            .to
+            .throw('cannot be empty');
+    });
 
-      
-  it('validateUser should throw when user with invalid email is passed', function() {
-    const user = {
-        username: 'validUsername',
-        email: 'invalidmail',
-        password: 'validPassword',
-        confirmPassword: 'validPassword',
-        secretQuestion: 'aValidQuestion',
-        secretAnswer: 'aValidAnswer'
-    };
+    it('validateUser should throw when user with invalid email is passed', function () {
+        const user = {
+            username: 'validUsername',
+            email: 'invalidmail',
+            password: 'validPassword',
+            confirmPassword: 'validPassword',
+            secretQuestion: 'aValidQuestion',
+            secretAnswer: 'aValidAnswer'
+        };
 
-    const action = function() {
-        userValidator().validateUser(user);
-    };
+        const action = function () {
+            userValidator().validateUser(user);
+        };
 
-    expect(action).to.throw('Invalid email');
-  });
+        expect(action)
+            .to
+            .throw('Invalid email');
+    });
 
-    
-  it('validateUser should throw when user with empty password is passed', function() {
-    const user = {
-        username: 'username',
-        email: 'valid@email.com',
-        password: '',
-        confirmPassword: '',
-        secretQuestion: 'aValidQuestion',
-        secretAnswer: 'aValidAnswer'
-    };
+    it('validateUser should throw when user with empty password is passed', function () {
+        const user = {
+            username: 'username',
+            email: 'valid@email.com',
+            password: '',
+            confirmPassword: '',
+            secretQuestion: 'aValidQuestion',
+            secretAnswer: 'aValidAnswer'
+        };
 
-    const action = function() {
-        userValidator().validateUser(user);
-    };
+        const action = function () {
+            userValidator().validateUser(user);
+        };
 
-    expect(action).to.throw('empty');
-  });
-    
-  it('validateUser should throw when user with short password is passed', function() {
-    const user = {
-        username: 'username',
-        email: 'valid@email.com',
-        password: '1',
-        confirmPassword: '1',
-        secretQuestion: 'aValidQuestion',
-        secretAnswer: 'aValidAnswer'
-    };
+        expect(action)
+            .to
+            .throw('empty');
+    });
 
-    const action = function() {
-        userValidator().validateUser(user);
-    };
+    it('validateUser should throw when user with short password is passed', function () {
+        const user = {
+            username: 'username',
+            email: 'valid@email.com',
+            password: '1',
+            confirmPassword: '1',
+            secretQuestion: 'aValidQuestion',
+            secretAnswer: 'aValidAnswer'
+        };
 
-    expect(action).to.throw('less than');
-  });
+        const action = function () {
+            userValidator().validateUser(user);
+        };
 
-    
-  it('validateUser should throw when user with unmatching password and confirm password is passed', function() {
-    const user = {
-        username: 'username',
-        email: 'valid@email.com',
-        password: 'validPassword',
-        confirmPassword: 'validPassword1',
-        secretQuestion: 'aValidQuestion',
-        secretAnswer: 'aValidAnswer'
-    };
+        expect(action)
+            .to
+            .throw('less than');
+    });
 
-    const action = function() {
-        userValidator().validateUser(user);
-    };
+    it('validateUser should throw when user with unmatching password and confirm passwor' +
+            'd is passed',
+    function () {
+        const user = {
+            username: 'username',
+            email: 'valid@email.com',
+            password: 'validPassword',
+            confirmPassword: 'validPassword1',
+            secretQuestion: 'aValidQuestion',
+            secretAnswer: 'aValidAnswer'
+        };
 
-    expect(action).to.throw('do not match');
-  });
+        const action = function () {
+            userValidator().validateUser(user);
+        };
 
-      
-  it('validateUser should throw when user with empty secret question is passed', function() {
-    const user = {
-        username: 'username',
-        email: 'valid@email.com',
-        password: 'validPassword',
-        confirmPassword: 'validPassword',
-        secretQuestion: '',
-        secretAnswer: 'aValidAnswer'
-    };
+        expect(action)
+            .to
+            .throw('do not match');
+    });
 
-    const action = function() {
-        userValidator().validateUser(user);
-    };
+    it('validateUser should throw when user with empty secret question is passed', function () {
+        const user = {
+            username: 'username',
+            email: 'valid@email.com',
+            password: 'validPassword',
+            confirmPassword: 'validPassword',
+            secretQuestion: '',
+            secretAnswer: 'aValidAnswer'
+        };
 
-    expect(action).to.throw('empty');
-  });  
+        const action = function () {
+            userValidator().validateUser(user);
+        };
 
-  it('validateUser should throw when user with short secret question is passed', function() {
-    const user = {
-        username: 'username',
-        email: 'valid@email.com',
-        password: 'validPassword',
-        confirmPassword: 'validPassword',
-        secretQuestion: 'Q?',
-        secretAnswer: 'aValidAnswer'
-    };
+        expect(action)
+            .to
+            .throw('empty');
+    });
 
-    const action = function() {
-        userValidator().validateUser(user);
-    };
+    it('validateUser should throw when user with short secret question is passed', function () {
+        const user = {
+            username: 'username',
+            email: 'valid@email.com',
+            password: 'validPassword',
+            confirmPassword: 'validPassword',
+            secretQuestion: 'Q?',
+            secretAnswer: 'aValidAnswer'
+        };
 
-    expect(action).to.throw('less than');
-  });
+        const action = function () {
+            userValidator().validateUser(user);
+        };
 
-       
-  it('validateUser should throw when user with empty secret answer is passed', function() {
-    const user = {
-        username: 'username',
-        email: 'valid@email.com',
-        password: 'validPassword',
-        confirmPassword: 'validPassword',
-        secretQuestion: 'aValidQuestion',
-        secretAnswer: ''
-    };
+        expect(action)
+            .to
+            .throw('less than');
+    });
 
-    const action = function() {
-        userValidator().validateUser(user);
-    };
+    it('validateUser should throw when user with empty secret answer is passed', function () {
+        const user = {
+            username: 'username',
+            email: 'valid@email.com',
+            password: 'validPassword',
+            confirmPassword: 'validPassword',
+            secretQuestion: 'aValidQuestion',
+            secretAnswer: ''
+        };
 
-    expect(action).to.throw('empty');
-  });
+        const action = function () {
+            userValidator().validateUser(user);
+        };
 
+        expect(action)
+            .to
+            .throw('empty');
+    });
 
-  it('validateUser should throw when user with short secret answer is passed', function() {
-    const user = {
-        username: 'username',
-        email: 'valid@email.com',
-        password: 'validPassword',
-        confirmPassword: 'validPassword',
-        secretQuestion: 'aValidQuestion',
-        secretAnswer: 'a.'
-    };
+    it('validateUser should throw when user with short secret answer is passed', function () {
+        const user = {
+            username: 'username',
+            email: 'valid@email.com',
+            password: 'validPassword',
+            confirmPassword: 'validPassword',
+            secretQuestion: 'aValidQuestion',
+            secretAnswer: 'a.'
+        };
 
-    const action = function() {
-        userValidator().validateUser(user);
-    };
+        const action = function () {
+            userValidator().validateUser(user);
+        };
 
-    expect(action).to.throw('less than');
-  });
+        expect(action)
+            .to
+            .throw('less than');
+    });
 });
 
-
-
-
-
-
-mocha.run();
